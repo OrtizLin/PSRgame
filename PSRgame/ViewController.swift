846cc923861b7e1b8a03c4d505db3e944ad0def1
otisHome <chenghsien852@gmail.com>
2019-04-13 11:52:45 +0800

    Initial Commit

---
+++ PSRgame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PSRgame
+//
+//  Created by Otis on 2019/4/13.
+//  Copyright © 2019年 Otis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
